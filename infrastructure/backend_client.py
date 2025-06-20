dd87023b131d927d6d506181c29c19e231b68887
Santiago Mickelle Olivera Guerra <150336891+FORASTERRO@users.noreply.github.com>
2025-06-19 20:56:21 -0500

    Updated reserved func

--- infrastructure/backend_client.py
+++ infrastructure/backend_client.py
@@ -9,3 +9,13 @@ class BackendClient:
         response = requests.post(self.backend_url, json=data, timeout=5)
         logging.info(f"Respuesta del backend: {response.status_code} - {response.text}")
         return response
+
+    def is_device_reserved(self, device_id):
+        url = f"{self.backend_url}/{device_id}/is-reserved"
+        try:
+            response = requests.get(url, timeout=5)
+            response.raise_for_status()
+            return response.json()  # True o False
+        except Exception as e:
+            logging.warning(f"No se pudo verificar si el dispositivo está reservado: {e}")
+            return False  # asume que no está reservado si falla
